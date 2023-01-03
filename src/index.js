5188bcb3693cd53ccecbc79a6a6db177ad4a1c59
nataliemezher <nataliemezher96@hotmail.com>
2023-01-03 18:39:28 +0100

    adds import for css style sheets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './style/pokemon_card.css';
+import './style/pokemon_content.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
